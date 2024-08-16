e4867e772f52c7f02a2e87738b3980894dc692cd
Chay06 <chaithanyakotagiri06@gmail.com>
2024-08-16 17:42:57 +0530

    first trial

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+function Footer() {
+    return (
+        <footer>
+            <p>
+                Created with ❤️ by &nbsp;
+                <p className="website-link">
+                    Chaithanya Kotagiri
+                </p>
+            </p>
+        </footer>
+    );
+}
+
+export default Footer;
